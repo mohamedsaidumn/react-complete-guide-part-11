ae1084c61b11782c07be327f508291f22cb5f03f
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-04-07 11:28:50 -0500

    fetching user unput using onchange/state or useRef

--- src/components/SimpleInput.tsx
+++ src/components/SimpleInput.tsx
@@ -1,11 +1,43 @@
+import { useRef, useState, FormEvent, SyntheticEvent } from "react";
+
 interface SimpleInputProps {}
 
 const SimpleInput = (props: SimpleInputProps) => {
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const [enteredName, setEnteredName] = useState<string>("");
+
+  const nameInputChangeHandler = (event: SyntheticEvent) => {
+    let target: HTMLInputElement = event.target as HTMLInputElement;
+    setEnteredName(target.value);
+  };
+
+  const formSubmissionHandler = (event: FormEvent) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+
+    if (nameInputRef === null || nameInputRef.current === null) {
+      return;
+    }
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    //nameInputRef.current.value = ""; //=> NOT IDEAL, DON'T DIRECTLY MANIPULATE THE DOM, LET REACT DO THAT FOR YOU
+    setEnteredName("");
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
