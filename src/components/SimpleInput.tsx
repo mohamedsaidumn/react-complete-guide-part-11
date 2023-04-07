a63279877ac9932a78fcfa0d00ce281a74f816c1
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-04-07 12:46:27 -0500

    show error after the user blur the input meaning he clicks it and clicks out.

--- src/components/SimpleInput.tsx
+++ src/components/SimpleInput.tsx
@@ -1,17 +1,36 @@
-import { useRef, useState, FormEvent, SyntheticEvent } from "react";
+import { useEffect, useState, FormEvent, SyntheticEvent } from "react";
 import classes from "./Tasks.module.css";
 
 interface SimpleInputProps {}
 
 const SimpleInput = (props: SimpleInputProps) => {
-  const nameInputRef = useRef<HTMLInputElement>(null);
   const [enteredName, setEnteredName] = useState<string>("");
   const [enteredNameIsValid, setEnteredNameIsValid] = useState<boolean>(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState<boolean>(false);
 
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name Input is valid!");
+    }
+  }, [enteredNameIsValid]);
+
   const nameInputChangeHandler = (event: SyntheticEvent) => {
     let target: HTMLInputElement = event.target as HTMLInputElement;
     setEnteredName(target.value);
+
+    if (target.value !== "") {
+      setEnteredNameIsValid(true);
+      return;
+    }
+  };
+
+  const nameInputBlurHandler = (event: SyntheticEvent) => {
+    setEnteredNameTouched(true); //if the input got blurred it was once touched
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
   };
 
   const formSubmissionHandler = (event: FormEvent) => {
@@ -19,19 +38,12 @@ const SimpleInput = (props: SimpleInputProps) => {
 
     setEnteredNameTouched(true);
 
-    if (nameInputRef === null || nameInputRef.current === null) {
-      return;
-    }
-
     if (enteredName.trim() === "") {
       setEnteredNameIsValid(false);
       return;
     }
 
     setEnteredNameIsValid(true);
-
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
 
     //nameInputRef.current.value = ""; //=> NOT IDEAL, DON'T DIRECTLY MANIPULATE THE DOM, LET REACT DO THAT FOR YOU
     setEnteredName("");
@@ -48,10 +60,10 @@ const SimpleInput = (props: SimpleInputProps) => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
         {nameInputIsInvalid && (
